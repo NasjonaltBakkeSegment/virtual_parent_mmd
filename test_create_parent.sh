a643fea1ad3b8f478fc3027587b23fed6a5bbd56
Luke Marsden <lhmarsden@hotmail.com>
2024-03-06 13:07:06 +0100

    bash script for testing building parent

---
+++ test_create_parent.sh
@@ -0,0 +1,4 @@
+# Script for generating a parent XML file from 2 children
+
+python3 virtual_parent_mmd.py --parent test_parent.xml --child S1A_EW_GRDH_1SDH_20231010T080117_20231010T080322_050700_061BD5_1AFA.xml
+python3 virtual_parent_mmd.py --parent test_parent.xml --child S1A_EW_GRDH_1SDH_20231016T071258_20231016T071502_050787_061EE6_78D6.xml
