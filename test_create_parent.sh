c3fd7352efb8970077cc54e4003368e5a2cab79f
Luke Marsden <lukem@met.no>
2024-03-25 10:52:30 +0100

    bulk testing on S1A products

--- test_create_parent.sh
+++ test_create_parent.sh
@@ -1,7 +1,10 @@
 # Script for generating a parent XML file from 2 children
 
-python3 virtual_parent_mmd.py --child S1A_EW_GRDM_1SDH_20230718T080105_20230718T080209_049475_05F2F9_D2FD.xml
-python3 virtual_parent_mmd.py --child S1A_EW_GRDM_1SDH_20221231T081230_20221231T081330_046573_0594D1_C33A.xml
-#python3 virtual_parent_mmd.py --child S1A_EW_GRDM_1SDH_20221231T081330_20221231T081430_046573_0594D1_CFA2.xml
+# Set the directory path
+directory="/home/lukem/Documents/MET/Projects/ESA_NBS/Git_repos/nbs-md-records/S1A/"
 
-
+# Find all XML files recursively in the directory and loop through them
+find "$directory" -type f -name '*.xml' | while read -r xml_file_path; do
+    xml_file=$(basename "$xml_file_path")
+    python3 virtual_parent_mmd.py --child "$xml_file"
+done
