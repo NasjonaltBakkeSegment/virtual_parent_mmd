c7edb48f7bc1a4f23a824b0439389b76ab90f043
nbs <nbs-helpdesk@met.no>
2024-04-17 13:51:45 +0000

    adding lines for qsub

--- run_for_all_files.sh
+++ run_for_all_files.sh
@@ -1,3 +1,19 @@
+#!/bin/bash -f
+#$ -N parent_mmd
+#$ -S /bin/bash
+#$ -pe shmem-1 1
+#$ -l h_rss=4G,mem_free=4G,h_data=4G 
+#$ -q research-r8.q
+#$ -M lukem@met.no
+#$ -j y
+#$ -m ba
+#$ -wd /home/nbs/file_conversion_r8/virtual_parent_mmd/OUT.$JOB_ID
+#$ -o /home/nbs/file_conversion_r8/virtual_parent_mmd/OUT.$JOB_ID
+#$ -e /home/nbs/file_conversion_r8/virtual_parent_mmd/OUT.$JOB_ID
+#$ -R y
+#$ -r y
+## ---------------------------
+
 # Script for linking all children to parents in the NBS mmd products directory
 
 config_file="config/config.yml"
