c1e9158edcbed0b67602ed1cbb4d504905b42014
nbs <nbs-helpdesk@met.no>
2024-05-10 08:09:47 +0000

    check orphans to for invalid titles and identifiers

--- check_orphans.sh
+++ check_orphans.sh
@@ -7,14 +7,12 @@
 #$ -M lukem@met.no
 #$ -j y
 #$ -m ba
-#$ -wd /home/nbs/file_conversion_r8/virtual_parent_mmd/OUT.$JOB_ID
-#$ -o /home/nbs/file_conversion_r8/virtual_parent_mmd/OUT.$JOB_ID
-#$ -e /home/nbs/file_conversion_r8/virtual_parent_mmd/OUT.$JOB_ID
+#$ -wd /home/nbs/file_conversion_r8/virtual_parent_mmd/check_orphans.$JOB_ID
+#$ -o /home/nbs/file_conversion_r8/virtual_parent_mmd/check_orphans.$JOB_ID
+#$ -e /home/nbs/file_conversion_r8/virtual_parent_mmd/check_orphans.$JOB_ID
 #$ -R y
 #$ -r y
 ## ---------------------------
-
-# Script for linking all children to parents in the NBS mmd products directory
 
 config_file="config/config.yml"
 directory=$(awk '/mmd_records_filepath:/ {print $2}' "$config_file")
@@ -22,5 +20,8 @@ directory=$(awk '/mmd_records_filepath:/ {print $2}' "$config_file")
 # Find all XML files recursively in the directory and loop through them
 find "$directory" -type f -name '*.xml' | while read -r xml_file_path; do
     xml_file=$(basename "$xml_file_path")
-    python3 link_child_with_parent.py --child "$xml_file"
+    # Check if the filename exists in orphans.txt
+    if grep -q "$xml_file" orphans.txt; then
+        python3 check_orphan.py --child "$xml_file"
+    fi
 done
--- run_for_one_file.sh
+++ run_for_one_file.sh
@@ -15,5 +15,5 @@
 ## ---------------------------
 
 # Script for linking all children to parents in the NBS mmd products directory
-python3 link_child_with_parent.py -c S2B_MSIL1C_20240423T125709_N0510_R038_T28WFE_20240423T135214
+python3 link_child_with_parent.py -c S1A_EW_GRDM_1SDH_20190119T181350_20190119T181450_025550_02D591_454C.xml
 
